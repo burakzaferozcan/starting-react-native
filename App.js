5d134031da0ade2469abcbecd701cb077ad4d517
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-12 18:38:52 +0300

    I messed with view, text and style

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.subContainer}>
+        <Text style={styles.textStyle}>Test text</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +16,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  subContainer: {
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 100,
+    height: 100,
+    borderRadius: 25,
+    borderWidth: 5,
+    borderColor: "red",
+  },
+  textStyle: {
+    color: "red",
+    fontWeight: "900",
   },
 });
