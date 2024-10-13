467936d65011072e719f811da71354c7e7f02a07
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-13 12:35:59 +0300

    Created LoginPage with Navigation

--- App.js
+++ App.js
@@ -1,110 +1,18 @@
-import { useState } from "react";
-import {
-  Image,
-  Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
-import Loading from "./src/components/Loading";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginPage from "./src/screens/LoginPage";
 
-export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [result, setResult] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+const Stack = createNativeStackNavigator();
 
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("./assets/images/login1.png")}
-        style={styles.loginImage}
-      />
-      <Text style={styles.title}>WELCOME</Text>
-      <Text style={styles.labelStyle}>Email</Text>
-      <TextInput
-        inputMode="email"
-        placeholder="Enter Your Email"
-        style={styles.textInputStyle}
-        onChangeText={setEmail}
-        value={email}
-      />
-      <Text style={styles.labelStyle}>Password</Text>
-      <TextInput
-        secureTextEntry={true}
-        placeholder="Enter Your Password"
-        style={styles.textInputStyle}
-        onChangeText={setPassword}
-        value={password}
-      />
-      <Pressable
-        style={({ pressed }) => [
-          styles.buttonStyle,
-          {
-            backgroundColor: pressed ? "#433878" : "#7E60BF",
-          },
-        ]}
-        onPress={() => setIsLoading(true)}
-      >
-        <Text style={styles.buttonTextStyle}>Save</Text>
-      </Pressable>
-      {isLoading ? (
-        <Loading changeIsLoading={() => setIsLoading(false)} />
-      ) : null}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFE1FF",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  labelStyle: {
-    textAlign: "justify",
-    width: "90%",
-    fontSize: 17,
-    fontWeight: "bold",
-    color: "#7E60BF",
-  },
-  textInputStyle: {
-    borderWidth: 1,
-    width: "90%",
-    height: 50,
-    borderRadius: 5,
-    marginVertical: 10,
-    textAlign: "center",
-    color: "#433878",
-    fontWeight: "bold",
-    borderColor: "#E4B1F0",
-  },
-  buttonStyle: {
-    alignItems: "center",
-    justifyContent: "center",
-    width: "90%",
-    height: 50,
-    borderRadius: 5,
-    marginVertical: 10,
-    textAlign: "center",
-  },
-  buttonTextStyle: {
-    color: "#fff",
-    fontWeight: "500",
-    fontSize: 17,
-  },
-  loginImage: {
-    width: 100,
-    height: 100,
-    objectFit: "cover",
-  },
-  title: {
-    color: "#433878",
-    fontSize: 20,
-    marginVertical: 20,
-    textAlign: "center",
-    fontWeight: "bold",
-  },
-});
+export default App;
---
+++ src/screens/LoginPage.js
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import Loading from "../components/Loading";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/images/login1.png")}
+        style={styles.loginImage}
+      />
+      <Text style={styles.title}>WELCOME</Text>
+      <Text style={styles.labelStyle}>Email</Text>
+      <TextInput
+        inputMode="email"
+        placeholder="Enter Your Email"
+        style={styles.textInputStyle}
+        onChangeText={setEmail}
+        value={email}
+      />
+      <Text style={styles.labelStyle}>Password</Text>
+      <TextInput
+        secureTextEntry={true}
+        placeholder="Enter Your Password"
+        style={styles.textInputStyle}
+        onChangeText={setPassword}
+        value={password}
+      />
+      <Pressable
+        style={({ pressed }) => [
+          styles.buttonStyle,
+          {
+            backgroundColor: pressed ? "#433878" : "#7E60BF",
+          },
+        ]}
+        onPress={() => setIsLoading(true)}
+      >
+        <Text style={styles.buttonTextStyle}>Save</Text>
+      </Pressable>
+      {isLoading ? (
+        <Loading changeIsLoading={() => setIsLoading(false)} />
+      ) : null}
+    </View>
+  );
+};
+export default LoginPage;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFE1FF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  labelStyle: {
+    textAlign: "justify",
+    width: "90%",
+    fontSize: 17,
+    fontWeight: "bold",
+    color: "#7E60BF",
+  },
+  textInputStyle: {
+    borderWidth: 1,
+    width: "90%",
+    height: 50,
+    borderRadius: 5,
+    marginVertical: 10,
+    textAlign: "center",
+    color: "#433878",
+    fontWeight: "bold",
+    borderColor: "#E4B1F0",
+  },
+  buttonStyle: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "90%",
+    height: 50,
+    borderRadius: 5,
+    marginVertical: 10,
+    textAlign: "center",
+  },
+  buttonTextStyle: {
+    color: "#fff",
+    fontWeight: "500",
+    fontSize: 17,
+  },
+  loginImage: {
+    width: 100,
+    height: 100,
+    objectFit: "cover",
+  },
+  title: {
+    color: "#433878",
+    fontSize: 20,
+    marginVertical: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
