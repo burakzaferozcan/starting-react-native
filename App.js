5c56d2d9d576ffd41713795a7ccaad6240d19a64
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-15 20:29:10 +0300

    redux installation and base store settings completed

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import RootNavigation from "./src/navigation/rootNavigation";
+import { Provider } from "react-redux";
+import { store } from "./src/redux/store";
 const UserStack = () => {
-  return <RootNavigation />;
+  return (
+    <Provider store={store}>
+      <RootNavigation />
+    </Provider>
+  );
 };
 export default UserStack;
