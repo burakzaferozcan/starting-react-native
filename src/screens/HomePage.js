10acb98ce4cc2d33b4c69ecfa399de5661f8c1e6
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-17 18:54:38 +0300

    data extraction from firebase has been implemented

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { collection, addDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 import CustomButton from "../components/CustomButton";
 
 const HomePage = () => {
+  const [data, setData] = useState([]);
+
   const sendData = async () => {
     try {
       const docRef = await addDoc(collection(db, "reactNativeLesson"), {
@@ -18,12 +20,29 @@ const HomePage = () => {
     }
   };
 
+  const getData = async () => {
+    const querySnapshot = await getDocs(collection(db, "reactNativeLesson"));
+    querySnapshot.forEach((doc) => {
+      setData([...data, doc.data()]);
+    });
+  };
   return (
     <View style={styles.container}>
       <Text>HomePage</Text>
       <CustomButton
         buttonText="Save"
         handleOnPress={sendData}
+        buttonTextColor="#fff"
+        buttonBackgroundColors={["#433878", "#7E60BF"]}
+      />
+      {data.map((item, index) => (
+        <Text key={index}>
+          {item.title} - {item.content} - Lesson: {item.lesson}
+        </Text>
+      ))}
+      <CustomButton
+        buttonText="Get Data"
+        handleOnPress={getData}
         buttonTextColor="#fff"
         buttonBackgroundColors={["#433878", "#7E60BF"]}
       />
