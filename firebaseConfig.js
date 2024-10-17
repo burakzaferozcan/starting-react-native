2389c908e7b4fd61afd3edcb2d17b7751db8b5b8
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-17 18:33:53 +0300

    data insertion to firebase has been applied

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +21,6 @@ const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
+export const db = getFirestore(app);
 
 export default app;
--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,10 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../firebaseConfig";
+import CustomButton from "../components/CustomButton";
 
 const HomePage = () => {
+  const sendData = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "reactNativeLesson"), {
+        title: "zero to hero",
+        content: "react native tutorial for beginner",
+        lesson: 95,
+      });
+      console.log("document written with id : ", docRef.id);
+    } catch (error) {
+      console.error("error adding document ", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>HomePage</Text>
+      <CustomButton
+        buttonText="Save"
+        handleOnPress={sendData}
+        buttonTextColor="#fff"
+        buttonBackgroundColors={["#433878", "#7E60BF"]}
+      />
     </View>
   );
 };
